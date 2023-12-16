39104c69424128d254c2bfcd3054d6477b6b01a7
Aum-Luhar <108570592+Aum-Luhar@users.noreply.github.com>
2023-12-16 19:59:23 +0530

    project.py

--- TASK-3/project.py
+++ TASK-3/project.py
@@ -1,1 +1,13 @@
+def contains_even_number(l): 
+    for ele in l: 
+        if ele % 2 == 0: 
+            print ("list contains an even number") 
+            break
+  
+    else:      
+        print ("list does not contain an even number") 
 
+ print ("For List 1:") 
+ contains_even_number([1, 4, 8]) 
+ print (" \nFor List 2:") 
+ contains_even_number([1, 3, 7])
