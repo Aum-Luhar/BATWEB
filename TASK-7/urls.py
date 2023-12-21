37bfc3a3166d10f1e84e3d28c774b09fe69ddce4
Aum-Luhar <108570592+Aum-Luhar@users.noreply.github.com>
2023-12-21 20:33:04 +0530

    Create urls.py

---
+++ TASK-7/urls.py
@@ -0,0 +1,9 @@
+# urls.py
+from django.urls import path
+from .views import edit_student, delete_student
+
+urlpatterns = [
+    # Other patterns
+    path('edit/<int:student_id>/', edit_student, name='edit_student'),
+    path('delete/<int:student_id>/', delete_student, name='delete_student'),
+]
